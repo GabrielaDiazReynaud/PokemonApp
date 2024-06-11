d3f867d646d40ebf66e5e4850705ba3612e88875
GabrielaDiazReynaud <gabydiaz2000@hotmail.com>
2024-06-10 23:14:38 -0600

    Image 404 eror  not loading resolved

--- src/components/NotFoundError.js
+++ src/components/NotFoundError.js
@@ -1,10 +1,11 @@
 import "../styles/errors.css";
+import ErrorImg from "../images/404.webp";
 function NotFoundError() {
   return (
     <div className="error-container">
       <div className="error">
         <p>4</p>
-        <img className="error-img" src="/assets/images/404.webp" alt="error" />
+        <img className="error-img" src={ErrorImg} alt="error" />
         <p>4</p>
       </div>
       <p className="error-title">Pokemon Not Found</p>
